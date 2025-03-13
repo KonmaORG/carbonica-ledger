f95269f0ce0d34a430d4b76897982caa38e18759
Cryptonean <faizanvit8@gmail.com>
2025-03-13 11:41:02 +0530

    crowfduing redeemer

--- src/types/redeemer.ts
+++ src/types/redeemer.ts
@@ -1,7 +1,15 @@
-import { Constr, Data, WalletApi } from "@lucid-evolution/lucid";
+import { Constr, Data, TLiteral, WalletApi } from "@lucid-evolution/lucid";
 import { AcceptRejectActionSchema, ActionSchema, OrefSchema } from "./cardano";
 
-//#region Redeemer
+export const PaymentKeyHashSchema = Data.Bytes();
+export const StakeKeyHashSchema = Data.Bytes();
+
+export const AddressSchema = Data.Tuple([
+  PaymentKeyHashSchema,
+  StakeKeyHashSchema,
+]);
+
+//#region Karbon ledger
 
 export const KarbonRedeemerSpendSchema = Data.Object({
   action: AcceptRejectActionSchema,
@@ -33,3 +41,48 @@ export const IdentificationRedeemer =
   IdentificationRedeemerSchema as unknown as IdentificationRedeemer;
 
 // #endregion
+
+//#region Karbon Crowdfuding
+
+export type CampaignState = "Initiated" | "Running" | "Cancelled" | "Finished";
+
+export const CampaignState: Record<
+  CampaignState,
+  { Title: CampaignState; Schema: TLiteral<CampaignState>; Constr: Constr<[]> }
+> = {
+  Initiated: {
+    Title: "Initiated",
+    Schema: Data.Literal("Initiated"),
+    Constr: new Constr(0, []),
+  },
+  Running: {
+    Title: "Running",
+    Schema: Data.Literal("Running"),
+    Constr: new Constr(1, []),
+  },
+  Cancelled: {
+    Title: "Cancelled",
+    Schema: Data.Literal("Cancelled"),
+    Constr: new Constr(2, []),
+  },
+  Finished: {
+    Title: "Finished",
+    Schema: Data.Literal("Finished"),
+    Constr: new Constr(3, []),
+  },
+};
+
+export const CampaignStateSchema = Data.Enum([
+  CampaignState.Initiated.Schema,
+  CampaignState.Running.Schema,
+  CampaignState.Cancelled.Schema,
+  CampaignState.Finished.Schema,
+]);
+
+export const CampaignStateRedeemer = {
+  Initiated: Data.to(new Constr(0, [])),
+  Running: Data.to(new Constr(1, [])),
+  Cancelled: Data.to(new Constr(2, [])),
+  Finished: Data.to(new Constr(3, [])),
+  Released: Data.to(new Constr(4, [])),
+};
